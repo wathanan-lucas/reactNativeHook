583560cdce0e8a5b63c43b45ee2b3ac8b666dc36
Wathanan Lucas <wathanansantos@gmail.com>
2023-04-28 20:41:06 -0300

    State - step 1

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { 
+  Button,
+  Text,
+  View 
+} 
+from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+  const [texto, setTexto] = useState('Olá, React Native!!')
+  
+  return(
+    <View style={{width: '100%', alignItems: 'center', padding: 20}}>
+      <Text style={{width: '80%', borderColor: '#CCC', borderWidth: 1, textAlign: 'center',             backgroundColor: "#DDD", padding: 8, borderRadius: 8}}>
+          {texto}
+      </Text>
+      <Button style={{width: '100%'}}
+        title="OK"
+        onPress={() => setTexto("Olá, React Native atualizado!")}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
